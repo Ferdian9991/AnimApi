c03a6511f8ba2d37478a64b24bf78559f345ad37
Ferdian9991 <ferdisatria15@gmail.com>
2021-10-22 17:55:11 +0700

    optimize scrape

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -27,13 +27,41 @@ const UserAgent = async (page) => {
 
 const onload = {
     args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--disable-gpu'
+        '--autoplay-policy=user-gesture-required',
+        '--disable-background-networking',
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-breakpad',
+        '--disable-client-side-phishing-detection',
+        '--disable-component-update',
+        '--disable-default-apps',
+        '--disable-dev-shm-usage',
+        '--disable-domain-reliability',
+        '--disable-extensions',
+        '--disable-features=AudioServiceOutOfProcess',
+        '--disable-hang-monitor',
+        '--disable-ipc-flooding-protection',
+        '--disable-notifications',
+        '--disable-offer-store-unmasked-wallet-cards',
+        '--disable-popup-blocking',
+        '--disable-print-preview',
+        '--disable-prompt-on-repost',
+        '--disable-renderer-backgrounding',
+        '--disable-setuid-sandbox',
+        '--disable-speech-api',
+        '--disable-sync',
+        '--hide-scrollbars',
+        '--ignore-gpu-blacklist',
+        '--metrics-recording-only',
+        '--mute-audio',
+        '--no-default-browser-check',
+        '--no-first-run',
+        '--no-pings',
+        '--no-sandbox',
+        '--no-zygote',
+        '--password-store=basic',
+        '--use-gl=swiftshader',
+        '--use-mock-keychain',
     ],
     headless: true
 }
@@ -111,6 +139,11 @@ class MainController {
                     const browser = await puppeteer.launch(onload);
                     const page = await browser.newPage();
                     await UserAgent(page);
+                    await page.setRequestInterception(true)
+                    page.on('request', (request) => {
+                        if (request.resourceType() === 'image') request.abort()
+                        else request.continue()
+                    })
                     await page.goto(url, {
                         waitUntil: 'networkidle2',
                     });
@@ -138,6 +171,11 @@ class MainController {
                     const browser = await puppeteer.launch(onload);
                     const page = await browser.newPage();
                     await UserAgent(page);
+                    await page.setRequestInterception(true)
+                    page.on('request', (request) => {
+                        if (request.resourceType() === 'image') request.abort()
+                        else request.continue()
+                    })
                     await page.goto(url, {
                         waitUntil: 'networkidle2',
                     });
@@ -168,6 +206,11 @@ class MainController {
                     const browser = await puppeteer.launch(onload);
                     const page = await browser.newPage();
                     await UserAgent(page);
+                    await page.setRequestInterception(true)
+                    page.on('request', (request) => {
+                        if (request.resourceType() === 'image') request.abort()
+                        else request.continue()
+                    })
                     await page.goto(url, {
                         waitUntil: 'networkidle2',
                     });
@@ -195,6 +238,11 @@ class MainController {
                     const browser = await puppeteer.launch(onload);
                     const page = await browser.newPage();
                     await UserAgent(page);
+                    await page.setRequestInterception(true)
+                    page.on('request', (request) => {
+                        if (request.resourceType() === 'image') request.abort()
+                        else request.continue()
+                    })
                     await page.goto(url, {
                         waitUntil: 'networkidle2',
                     });
